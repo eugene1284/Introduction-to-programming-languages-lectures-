10869eb32d278ab492036518bc7fcbc7f10d5d3e
eugene_b <jenksbu@gmail.com>
2022-04-25 00:15:29 +0300

    add random

--- ex003/Program.cs
+++ ex003/Program.cs
@@ -1,3 +1,6 @@
-﻿int numberA = 3;
-int numberB = 4;
-Console.WriteLine(numberA + numberB);
+﻿int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9;
+int numberB = new Random().Next(1, 10); // 1 2 3 4 ... 9;
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
--- ex004Dif/Program.cs
+++ ex004Dif/Program.cs
@@ -1,3 +1,5 @@
-﻿double numberA = 12;
-double numberB = 5;
+﻿double numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
+double numberB = new Random().Next(1, 10); // 1 2 3 4 ... 9;
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
 Console.WriteLine(numberA / numberB);
